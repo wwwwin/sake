b9c657b043e9ad3ced9301f249352c01deb2ee35
“wwwwin” <newlife948@gmail.com>
2015-06-18 11:56:29 +0800

    controller fininsh

--- app/controllers/sakayas_controller.rb
+++ app/controllers/sakayas_controller.rb
@@ -1,34 +1,53 @@
 class SakayasController < ApplicationController
 
+before_action :set_event, :only => [ :show, :edit, :update, :destroy]	
+
 	def index
 		@sakayas = Sakaya.page(params[:page]).per(5)
 
 	end
 
 	def new
-
-		
+		@sakaya = Sakaya.new
 	end
 
 	def show
-		
+		@sakaya = Sakaya.find(params[:id])
 	end
 
 	def creat
-		
+		@sakaya = Sakaya.new(sakaya_params)
+  	if @sakaya.save
+  			flash[:notice] = "sakaya was successfully created1"
+    		redirect_to sakaya_url 
+  	else
+    		render :action => :new	
+	  end
 	end
-	def edit
-		
+	
+	def edit	
 	end
 
-	def update
-		
+	def update	
+		if @sakaya.update(sakaya_params)
+	    redirect_to events_url 
+	  else
+	    render :action => :edit
+	  end
 	end
+
 
 	def destory
-		
+		 @sakaya.destroy
+  		redirect_to :action => :index
+	end
+end
+private
+
+	def event_params
+  		params.require(:sakaya).permit(:title, :description)	
 	end
 
-
-
-end
+	def set_event
+  		@sakaya = Sakaya.find(params[:id])
+	end
