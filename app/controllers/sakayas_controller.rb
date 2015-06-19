b058f04968fc7421425f07493b81e325d0aa33e1
“wwwwin” <newlife948@gmail.com>
2015-06-20 00:56:47 +0800

    add gem sass

--- app/controllers/sakayas_controller.rb
+++ app/controllers/sakayas_controller.rb
@@ -2,51 +2,52 @@ class SakayasController < ApplicationController
 
 before_action :set_sakaya, :only => [ :show, :edit, :update, :destroy]
 
-	def index
-		@sakayas = Sakaya.page(params[:page]).per(5)
+  def index
+    @sakayas = Sakaya.page(params[:page]).per(5)
 
-	end
+  end
 
-	def new
-		@sakaya = Sakaya.new
-	end
+  def new
+    @sakaya = Sakaya.new
+  end
 
-	def show
-		@sakaya = Sakaya.find(params[:id])
-	end
+  def show
+    @sakaya = Sakaya.find(params[:id])
+  end
 
-	def create
-		@sakaya = Sakaya.new(sakaya_params)
-  	if @sakaya.save
-  			flash[:notice] = "sake was successfully created1"
-    		redirect_to sakayas_url
-  	else
-    		render :action => :new
-	  end
-	end
+  def create
+    @sakaya = Sakaya.new(sakaya_params)
+    if @sakaya.save
+      flash[:notice] = "sake was successfully created1"
+      redirect_to sakayas_url
+  else
+      render :action => :new
+  end
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-		if @sakaya.update(sakaya_params)
-	    redirect_to sakayas_url
-	  else
-	    render :action => :edit
-	  end
-	end
+  def update
+  if @sakaya.update(sakaya_params)
+    redirect_to sakayas_url
+  else
+    render :action => :edit
+  end
+  end
 
-	def destroy
-		 @sakaya.destroy
-  		 redirect_to :action => :index
-	end
-end
-private
+  def destroy
+    @sakaya.destroy
+    redirect_to :action => :index
+  end
+  end
 
-	def sakaya_params
-  		params.require(:sakaya).permit(:title, :date, :price, :description)
-	end
+  private
 
-	def set_sakaya
-  		@sakaya = Sakaya.find(params[:id])
-	end
+  def sakaya_params
+    params.require(:sakaya).permit(:title, :date, :price, :description)
+  end
+
+  def set_sakaya
+    @sakaya = Sakaya.find(params[:id])
+  end
